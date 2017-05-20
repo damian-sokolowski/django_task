3bd8451029d23015da32815ade39440e3966acf7
Damian Sokolowski <dammansokol@gmail.com>
2017-05-21 00:57:59 +0200

    Extended user model

--- users_list/admin.py
+++ users_list/admin.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from users_list.models import UserProfile
+
+
+class UserInline(admin.StackedInline):
+    model = UserProfile
+    list_display = ('birthday', 'random_number',)
+    can_delete = False
+    verbose_name_plural = 'users profile'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserInline, )
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('birthday', 'random_number')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
+
--- users_list/models.py
+++ users_list/models.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+import random
+
+
+def get_random_number():
+    return random.randint(1, 100)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile',)
+    birthday = models.DateField(blank=True, null=True,)
+    random_number = models.IntegerField(default=get_random_number,)
